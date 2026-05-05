916b304d9c579e83e3facac81a5b43f42bd18559
SB709 <sb709smi@UPCCAMPUS.LOCAL>
2026-05-04 20:35:34 -0500

    CI + CD

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest')
-const app = require('../app')
+const app = require('../App')
 
 test('GET / returns ok', async () => {
     const res = await request(app).get('/')
